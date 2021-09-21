97786f782be873ec7206bfaa48437f3ae9c701dd
DennisActa <assistenzatecnica.actainfo@gmail.com>
2021-09-21 16:49:31 +0200

    update

--- blog_api/serializers.py
+++ blog_api/serializers.py
@@ -12,4 +12,4 @@ class PostSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Post
-        fields = ('id', 'title', 'author', 'excerpt', 'content', 'status')
+        fields = ('category', 'id', 'title', 'slug', 'author', 'excerpt', 'content', 'status')
